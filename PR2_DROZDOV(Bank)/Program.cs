a300f62dc1d70b2a2e8e48f041eb1163eaf557b0
V01CE07 <114878496+V01CE07@users.noreply.github.com>
2023-10-26 14:39:17 +0300

    Add files via upload

---
+++ PR2_DROZDOV(Bank)/Program.cs
@@ -0,0 +1,33 @@
+﻿using PR_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchetVBanke USER1 = new(); //создание новых "пользователей"
+            SchetVBanke USER2 = new();
+            SchetVBanke USER3 = new();
+            //заполнение информации о пользователях
+            USER1.User(1488, "Drozdov Ivan Vitalievich", 5566);
+            USER2.User(1337, "Z Artem Z", 0);
+            USER3.User(228, "Kuzmin Nikolay Stanislavovich", 874);
+
+            USER1.Operations(USER2);
+            USER2.Operations(USER1);
+            USER3.Operations(USER2);
+            USER1.Operations(USER3);
+
+            
+
+
+
+        }
+    }
+}
